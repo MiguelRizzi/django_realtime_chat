d4d5f25d172442641f540dae7e98ba75f838744d
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2025-05-23 19:54:33 -0300

    Add app users

--- project/config/settings.py
+++ project/config/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- project/config/urls.py
+++ project/config/urls.py
@@ -22,6 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("users/", include(("users.urls", "users"))),
 ]
 
 if settings.DEBUG:
